4fece0a38e0c06a2e2c0d7c4fb097fe2d6969cbc
Nico <nico14@gmail.com>
2025-05-06 22:42:59 -0400

    base name lowercase

--- vite.config.ts
+++ vite.config.ts
@@ -23,5 +23,5 @@ export default defineConfig({
   esbuild: {
     target: 'esnext', // Use a modern target that supports top-level await
   },
-  base: '/BIM2GIS/', // Set the base path for the project
+  base: '/bim2gis/', // Set the base path for the project
 });
